d546c225e437b384aac43d70ac9fcafe69e64c00
= <james@thesugdens.com>
2017-12-07 22:52:18 +0000

    OSE V2 Added Main.h so no #pragma once in cpp file

--- OSE V2/Main.cpp
+++ OSE V2/Main.cpp
@@ -1,9 +1,4 @@
-#pragma once
-
-#include "stdafx.h"
-#include "Project.h"
-#include "ProjectLoaderXML.h"
-#include "Game.h"
+#include "Main.h"
 
 int main()
 {
---
+++ OSE V2/Main.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "stdafx.h"
+#include "Project.h"
+#include "ProjectLoaderXML.h"
+#include "Game.h"
+
+int main();
