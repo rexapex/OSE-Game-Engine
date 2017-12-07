97b47fed766aecfe0145fc3ded8f4e89273b98c0
= <james@thesugdens.com>
2017-12-07 22:46:50 +0000

    OSE V2 main function now returns 0 for g++ compatibility

--- OSE V2/Main.cpp
+++ OSE V2/Main.cpp
@@ -5,11 +5,12 @@
 #include "ProjectLoaderXML.h"
 #include "Game.h"
 
-void main()
+int main()
 {
 	using namespace origami_sheep_engine;
 	std::unique_ptr<Game> game = std::make_unique<Game>();
 	game->loadProject("TestProject");
 	game->loadScene("scene1");
 	getchar();
+	return 0;
 }
