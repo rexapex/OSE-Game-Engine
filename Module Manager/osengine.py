a24d006ba5ace85163dd2463ee0718b9e4e94ab8
= <james@thesugdens.com>
2017-12-28 23:10:15 +0000

    OSE V2 Module manager basic profile management

--- Module Manager/osengine.py
+++ Module Manager/osengine.py
@@ -63,6 +63,39 @@ def addudir(command):
 
 
 
+#TODO - fetch code from remote repositories to update profiles' modules
+def updateprofile(command):
+    pass
+
+
+
+#make a new engine profile
+def mkprofile(command):
+    #shlex split preserves quoted strings
+    try:
+        args = shlex.split(command, posix=False)        #TODO - test this on MACOS and Linux
+        if len(args) > 1:
+            name = args[1]
+            if "profiles" in contentsObj:
+                contentsObj["profiles"][name] = {"modules": {}}
+            else:
+                contentsObj["profiles"][name] = {"modules" : {}}
+            if len(args) > 2:
+                blueprint = args[2]
+                #if the blueprint is std, then get from the standard module git repository
+                if blueprint == "std":
+                    contentsObj["profiles"][name]["modules"]["ProjectLoader"] = {"name": "ProjectLoaderXML", "dependencies": {"rapidxml": {"download": "https://sourceforge.net/projects/rapidxml/files/latest/download"}}}
+                    contentsObj["profiles"][name]["modules"]["WindowManager"] = {"name": "WindowManagerGLFW", "dependencies": {"GLFW": {"repo": "https://github.com/glfw/glfw.git", "branch": "master"}}}
+                    updateprofile("update " + name)
+            writeSettingsFile()
+            print("created profile " + name)
+        else:
+            print("error: mkprofile requires 1 arg, <name>")
+    except ValueError:
+        print("error: name and/or blueprint is corrupt")
+
+
+
 #TODO - this function is probably quite important
 def build(command):
     #first, generate the EngineReferences.h file
@@ -76,7 +109,9 @@ def parseCommand(command):
         print("quit                                  quit and return the the command prompt")
         print("lsdirs                                list all currently configured user directories")
         print("addudir <directory path>              add a directory to the list of user directories")
-        print("build                                 TODO - should build OSE with listed modules")
+        print("mkprofile <name> <blueprint>          make a new profile, name should be unique, blueprint details the default set of modules, use <blueprint> = std to download the default modules")
+        print("update <profile name>                 update modules for the specified profile")
+        print("build <profile name>                  TODO - should build OSE with listed modules")
     elif command == "lsdirs":
         if not ((not "user_dirs" in contentsObj) or (not isinstance(contentsObj["user_dirs"], list))):
             for dir in contentsObj["user_dirs"]:
@@ -84,7 +119,15 @@ def parseCommand(command):
     elif command.startswith("addudir"):
         addudir(command)
     elif command == "lsprofiles":
-        print
+        if not ((not "profiles" in contentsObj) or (not isinstance(contentsObj["profiles"], list))):
+            for profile in contentsObj["profiles"]:
+                print("PROFILE " + profile["name"])
+                for module in profile["modules"]:
+                    print("    MODULE " + module + ": " + profile["modules"][module]["name"])
+    elif command.startswith("mkprofile"):
+        mkprofile(command)
+    elif command.startswith("update"):
+        updateprofile(command)
     elif command == "gettest":
         if not ((not "user_dirs" in contentsObj) or (not isinstance(contentsObj["user_dirs"], list)) or (len(contentsObj["user_dirs"]) == 0)):
             subprocess.call(["git", "clone", "https://github.com/rexapex/OSE-V2-TestProject.git", contentsObj["user_dirs"][0]+"/Projects/OSE-V2-TestProject"])
@@ -98,7 +141,7 @@ def parseCommand(command):
 #loops until the exit command is entered
 def loop():
     while True:
-        command = input("Enter a command: ")
+        command = input("> ")
         parseCommand(command)
 
 
