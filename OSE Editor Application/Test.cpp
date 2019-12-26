9481ea0a43108053c16c65fee63762c78a6ae54f
rexapex <james@thesugdens.com>
2019-12-26 21:40:31 +0000

    Updated test scripts

--- OSE Editor Application/Test.cpp
+++ OSE Editor Application/Test.cpp
@@ -18,15 +18,9 @@ DATA
 // Define the engine used to process the pool of custom component objects
 ENGINE
 (
-	void AddCustomComponent(Entity & entity, CustomComponent & comp)
-	{
-		std::cout << "Adding custom component\n";
-		data_array_.emplace_back(PlayerMovementData{entity});
-	}
-
 	void Init()
 	{
-		
+		std::cout << "Initialising custom engine\n";
 	}
 
 	void Update()
@@ -34,7 +28,13 @@ ENGINE
 		for(auto & data : data_array_)
 		{
 			data.entity.Translate2d(0.1f, 0);
-			std::cout << "Entity Translation: " << data.entity.GetLocalTransform().GetPosition().x << "\n";
+		//	std::cout << "Entity Translation: " << data.entity.GetLocalTransform().GetPosition().x << "\n";
 		}
+	}
+
+	void AddCustomComponent(Entity & entity, CustomComponent & comp)
+	{
+		std::cout << "Adding custom component\n";
+		data_array_.emplace_back(PlayerMovementData{entity});
 	}
 )
