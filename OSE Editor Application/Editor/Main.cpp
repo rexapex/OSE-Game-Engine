3cf0209d75214eb9590cb1a8aab64ab916dd78b1
rexapex <james@thesugdens.com>
2019-12-26 14:32:39 +0000

    Removed old uncommented test code from editor main

--- OSE Editor Application/Editor/Main.cpp
+++ OSE Editor Application/Editor/Main.cpp
@@ -8,16 +8,6 @@ int main(int argc, char * argv[])
 {
 	using namespace ose;
 
-	///using namespace ose::math;
-	///Transform t { { 0, 832043982, -432098 }, { 0, 0, 0 }, { 1, 1, 1 } };
-	///t.rotate(3.14159265359, 0, 0);
-	///t.rotateDeg({180, 0, 0});
-	///DEBUG_LOG(t.get_up().x << ", " << t.get_up().y << ", " << t.get_up().z);
-	///DEBUG_LOG(t.get_forward().x << ", " << t.get_forward().y << ", " << t.get_forward().z);
-	///DEBUG_LOG(t.get_right().x << ", " << t.get_right().y << ", " << t.get_right().z);
-
-	/// std::unique_ptr<Game> game = std::make_unique<Game>();
-	
 	// TODO - might need to destroy resources before returning error
 	
 	// load the project, giving access to all of its settings and scenes
