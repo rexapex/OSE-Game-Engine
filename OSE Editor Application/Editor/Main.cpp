1320e5c99e2ada91faab537316e0bf13ad5eb2a7
rexapex <james@thesugdens.com>
2020-01-04 12:41:17 +0000

    Refactor - Removed Env and static instance of Game.

--- OSE Editor Application/Editor/Main.cpp
+++ OSE Editor Application/Editor/Main.cpp
@@ -1,6 +1,5 @@
 #include "stdafx.h"
 #include "Main.h"
-#include "OSE-Core/Env.h"
 #include "OSE-Core/Game/Game.h"
 
 int main(int argc, char * argv[])
@@ -9,9 +8,12 @@ int main(int argc, char * argv[])
 
 	// TODO - might need to destroy resources before returning error
 
+	// Create a game object
+	auto game = std::make_unique<Game>();
+
 	// load the project, giving access to all of its settings and scenes
 	try {
-		Env::GetGame().LoadProject("OSE-TestProject");
+		game->LoadProject("OSE-TestProject");
 	} catch(const std::invalid_argument & e) {
 		ERROR_LOG(e.what());
 		return 1;
@@ -23,7 +25,7 @@ int main(int argc, char * argv[])
 
 	// load a scene, giving access to all of its entities and resources
 	try {
-		Env::GetGame().LoadScene("scene1");
+		game->LoadScene("scene1");
 	} catch(const std::invalid_argument & e) {
 		ERROR_LOG(e.what());
 		return 1;
@@ -35,7 +37,7 @@ int main(int argc, char * argv[])
 
 	// set the new scene as the active scene
 	try {
-		Env::GetGame().SetActiveScene("scene1");
+		game->SetActiveScene("scene1");
 	} catch(const std::invalid_argument & e) {
 		ERROR_LOG(e.what());
 		getchar();
@@ -43,7 +45,7 @@ int main(int argc, char * argv[])
 	}
 
 	// all resources have been loaded and entities initialised, therefore, start the game
-	Env::GetGame().StartGame();
+	game->StartGame();
 	
 	return 0;
 }
