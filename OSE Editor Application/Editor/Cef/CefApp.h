18992e44439ee695e09c3f6011075b9f03f3ab04
rexapex <james@thesugdens.com>
2020-01-05 14:14:19 +0000

    Added cef files

---
+++ OSE Editor Application/Editor/Cef/CefApp.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2013 The Chromium Embedded Framework Authors. All rights
+// reserved. Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+
+#ifndef CEF_TESTS_CEFSIMPLE_SIMPLE_APP_H_
+#define CEF_TESTS_CEFSIMPLE_SIMPLE_APP_H_
+
+#include "include/cef_app.h"
+#include "Editor/Controller/Controller.h"
+
+// Implement application-level callbacks for the browser process.
+class SimpleApp : public CefApp, public CefBrowserProcessHandler {
+public:
+	SimpleApp();
+
+	// CefApp methods:
+	virtual CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler()
+		OVERRIDE {
+		return this;
+	}
+
+	// CefBrowserProcessHandler methods:
+	virtual void OnContextInitialized() OVERRIDE;
+
+private:
+	// Include the default reference counting implementation.
+	IMPLEMENT_REFCOUNTING(SimpleApp);
+
+	// Game object used to simplify interface between OSE and editor
+	std::unique_ptr<ose::editor::Controller> controller_;
+};
+
+#endif  // CEF_TESTS_CEFSIMPLE_SIMPLE_APP_H_
---
+++ OSE Editor Application/Editor/Cef/RenderHandler.h
@@ -0,0 +1,8 @@
+#pragma once
+/*#include "include/cef_render_handler.h"
+
+class RenderHandler : public CefRenderHandler
+{
+	
+};
+*/
