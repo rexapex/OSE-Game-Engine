93c9188286971c2c7469e10546ff1dd34a5f20ec
rexapex <james@thesugdens.com>
2020-02-25 20:36:08 +0000

    Removed ability to add arbitrary entity objects to entity lists. Entities must now be constructed in-place within the entity lists. Remove ability to remove entity from entity list by name (since name is not unique).

--- OSE V2/OSE-Core/Entity/EntityList.cpp
+++ OSE V2/OSE-Core/Entity/EntityList.cpp
@@ -16,54 +16,25 @@ namespace ose
 			this->entities_.push_back(std::make_unique<Entity>(*e));
 		}
 	}
-	
-	// add an entity to the entity list
-	// name must be unique
-	// method constructs a new object
-	// method takes an array of constructor arguments
-	template<typename... Args>
-	Entity & EntityList::AddEntity(Args &&... params)
+
+	// Add an entity to the entity list
+	// New entity is a deep copy of the entity passed
+	// Method constructs a new object
+	// Returns a reference to the newly created entity
+	unowned_ptr<Entity> EntityList::AddEntity(const Entity & other)
 	{
 		// construct a new entity object
 		try {
-			entities_.emplace_back( std::make_unique<Entity>(std::forward<Args>(params)...) );
-			return *entities_.back();	// if emplace_back succeeds, the new entity will be the last entity
-		} catch(std::exception & e) {
-			throw e;
+			return entities_.emplace_back( std::make_unique<Entity>(other) ).get();
+		} catch(...) {
+			return nullptr;
 		}
 	}
 
-	// add an entity to the entity list
-	// method moves the object passed
-	void EntityList::AddEntity(std::unique_ptr<Entity> e)
-	{
-		try {
-			// move the entity pointer to the list of entities
-			entities_.emplace_back( std::move(e) );
-		} catch(std::exception & e) {
-			throw e;
-		}
-	}
-
-
-	// add an entity to the entity list
-	// new entity is a deep copy of the entity passed
-	// method constructs a new object
-	// returns: reference to newly created entity
-	Entity & EntityList::AddEntity(const Entity & other)
-	{
-		// construct a new entity object
-		try {
-			entities_.emplace_back( std::make_unique<Entity>(other) );
-			return *entities_.back();	// if emplace_back succeeds, the new entity will be the last entity
-		} catch(std::exception & e) {
-			throw e;
-		}
-	}
-
-	// remove sub entity
-	// return true if sub entity is removed
-	// return false if the sub entity does not belong to this entity
+	// TODO - NEEDS SERIOUS TESTING, NO IDEA WHETHER THIS WORKS
+	// Remove an entity from the entity list
+	// Return true if entity is removed
+	// Return false if the entity does not belong to this entity list
 	bool EntityList::RemoveEntity(const Entity & entity)
 	{
 		// no sub entity can be removed if there are no sub entities, therefore return false
@@ -81,36 +52,9 @@ namespace ose
 		return (size_before != entities_.size());
 	}
 
-	// remove entity by name
-	// return true if entity with given name is removed
-	// return false if no entity with given name exists
-	bool EntityList::RemoveEntity(const std::string & name)
-	{
-		// no sub entity can be removed if there are no entities, therefore return false
-		if(entities_.empty()) {
-			return false;
-		}
-
-		// otherwise, find the entity with the given name
-		// search from beginning to end of list
-		// return first component to return true from lambda
-		auto & pos = std::find_if(entities_.begin(), entities_.end(), [name] (auto & entity) {
-			return entity->GetName() == name;
-		});
-
-		// if a matching entity is found, remove it then return true
-		if(pos != entities_.end()) {
-			entities_.erase(pos);
-			return true;
-		}
-
-		// else, return false
-		return false;
-	}
-
-	// remove entity by EntityID
-	// return true if entity with given EntityID is removed
-	// return false if no entity with given EntityID exists
+	// Remove entity by EntityID
+	// Return true if entity with given EntityID is removed
+	// Return false if no entity with given EntityID exists in this entity list
 	bool EntityList::RemoveEntity(const EntityID uid)
 	{
 		// no entity can be removed if there are no sub entities, therefore return false
