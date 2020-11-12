33ee651046bd765911ff5101bb515d384cb79bc4
rexapex <james@thesugdens.com>
2020-11-12 21:13:13 +0000

    Added uptr, sptr, wptr, make_unique and make_shared to the ose namespace (current version uses std functions/types).

--- OSE V2/OSE-Core/Types.h
+++ OSE V2/OSE-Core/Types.h
@@ -2,5 +2,24 @@
 
 namespace ose
 {
-	
+	template <typename T>
+	using uptr = std::unique_ptr<T>;
+
+	template <typename T>
+	using sptr = std::shared_ptr<T>;
+
+	template <typename T>
+	using wptr = std::weak_ptr<T>;
+
+	template <typename T, typename... Args>
+	inline uptr<T> make_unique(Args &&... args)
+	{
+		return std::make_unique<T>(std::forward<Args>(args)...);
+	}
+
+	template <typename T, typename... Args>
+	inline sptr<T> make_shared(Args &&... args)
+	{
+		return std::make_shared<T>(std::forward<Args>(args)...);
+	}
 }
